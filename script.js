38f5f0e4059a3b506a8d8e7231c6fc010a7e2acd
cadeslayden <cslayden3@gmail.com>
2022-11-27 21:59:50 -0700

    final website

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript"
 document.querySelector("body").appendChild(h2)
+const img
+.center img {        
+    display:block;
+    margin-left:auto;
+    margin-right:auto;
+  }
